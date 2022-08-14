a087fb8052693f0c4b43ea82890ea2d060764e9c
Atn_25 <93600196+atn25042002@users.noreply.github.com>
2022-08-13 21:54:20 -0500

    Base2

--- PlagiarismChecker.java
+++ PlagiarismChecker.java
@@ -1,8 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
 public class PlagiarismChecker{  
-    private 
+    private ArrayList<String> bd;
+
+    public static void main(String[] args){
+        System.out.print("Prueba");
+    }
 
     public boolean loadFiles(String[] paths){
+        bd= new ArrayList<String>();
 
+        String line;
+        try {
+            Scanner lector= new Scanner(new FileReader("Mensaje.txt"));
+            while(lector.hasNextLine()) {
+                line= lector.nextLine();
+                System.out.println(line);
+            }
+            lector.close();
+        }
+        catch (FileNotFoundException e){
+            System.out.print("Error");
+            return false;
+        }
     }
 
     public ResultChecker verifyPlagiarism(String path){
