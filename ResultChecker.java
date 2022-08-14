6975bfffbba60ee13ce828ae4d0c1519897931e7
Claudia Contreras Mamani <ccontrerasma@unsa.edu.pe>
2022-08-13 23:01:29 -0500

    New

--- ResultChecker.java
+++ ResultChecker.java
@@ -1,8 +1,8 @@
 public class ResultChecker{   
-    public boolean[] result;
+	public boolean[] result;
 
     public void comprobarPlagio(){
         //Comprueba
-        this.result=[];
+       System.out.println(this.result);;
     }
 }
