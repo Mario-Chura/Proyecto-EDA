e8d632816f22a0f213d6d6eb5d9a45fc0fc749ec
Atn_25 <93600196+atn25042002@users.noreply.github.com>
2022-08-13 21:39:16 -0500

    Archivos base

---
+++ PlagiarismChecker.java
@@ -0,0 +1,13 @@
+public class PlagiarismChecker{  
+    private 
+
+    public boolean loadFiles(String[] paths){
+
+    }
+
+    public ResultChecker verifyPlagiarism(String path){
+        ResultChecker obj= null;
+        
+        return obj.result;
+    }
+}
---
+++ ResultChecker.java
@@ -0,0 +1,8 @@
+public class ResultChecker{   
+    public boolean[] result;
+
+    public void comprobarPlagio(){
+        //Comprueba
+        this.result=[];
+    }
+}
